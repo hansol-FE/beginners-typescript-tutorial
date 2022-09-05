eb361a1966ae2209495f81cbb6f88c7245d82e5a
hansolpark <hansol94@seerslab.com>
2022-09-05 10:23:29 +0900

    solve ex15

--- src/15-intersection.problem.ts
+++ src/15-intersection.problem.ts
@@ -2,7 +2,6 @@ interface User {
   id: string;
   firstName: string;
   lastName: string;
-  posts: Post[];
 }
 
 interface Post {
@@ -15,7 +14,7 @@ interface Post {
  * How do we type this return statement so it's both
  * User AND { posts: Post[] }
  */
-export const getDefaultUserAndPosts = (): User => {
+export const getDefaultUserAndPosts = (): User & { posts: Post[] } => {
   return {
     id: "1",
     firstName: "Matt",
